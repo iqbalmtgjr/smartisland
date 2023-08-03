2b0d4f3c97636e5fca31db22ea8f980c4171fa92
Muhammad Iqbal <iqbalmtgjr@gmail.com>
2023-08-03 10:02:28 +0700

    ubah sikit

--- resources/views/livewire/kelola-maps.blade.php
+++ resources/views/livewire/kelola-maps.blade.php
@@ -14,19 +14,22 @@
                             <button class="btn btn-primary mb-2" data-toggle="modal" data-target="#tambah">+
                                 Tambah Data Gis</button>
                         </div>
-                        {{-- <hr> --}}
                         <div class="row">
                             <div class="col">
-                                <select wire:model="paginate" name="" id="" class="form-control form-control-sm sm w-auto">
+                                <select wire:model="paginate" name="" id=""
+                                    class="form-control form-control-sm sm w-auto">
                                     <option value="5">5</option>
                                     <option value="10">10</option>
                                     <option value="15">15</option>
                                 </select>
                             </div>
                             <div class="col">
-                                <input type="text" wire:model="search" class="form-control form-control-sm w-auto float-right" placeholder="Cari">
+                                <input type="text" wire:model="search"
+                                    class="form-control form-control-sm w-auto float-right" placeholder="Cari">
                             </div>
                         </div>
+                        <hr>
+
                         <table class="table">
                             <thead>
                                 <tr>
@@ -37,7 +40,7 @@
                                     <th>Aksi</th>
                                 </tr>
                             </thead>
-                            
+
                             <tbody>
                                 @foreach ($locations as $item)
                                     <tr>
@@ -64,30 +67,30 @@
                 </div>
             </div>
         </div>
-    @push('footer')
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <script type="text/javascript">
-            document.addEventListener('DOMContentLoaded', function() {
-                @this.on('triggerDelete', locationId => {
-                    Swal.fire({
-                        title: 'Yakin?',
-                        text: "Mau Hapus Data Ini?",
-                        icon: 'warning',
-                        showCancelButton: true,
-                        confirmButtonColor: '#3085d6',
-                        cancelButtonColor: '#d33',
-                        confirmButtonText: 'Ya',
-                    }).then((result) => {
-                        //if user clicks on delete
-                        if (result.value) {
-                            // calling destroy method to delete
-                            @this.call('delete', locationId)
-                        }
+        @push('footer')
+            <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+            <script type="text/javascript">
+                document.addEventListener('DOMContentLoaded', function() {
+                    @this.on('triggerDelete', locationId => {
+                        Swal.fire({
+                            title: 'Yakin?',
+                            text: "Mau Hapus Data Ini?",
+                            icon: 'warning',
+                            showCancelButton: true,
+                            confirmButtonColor: '#3085d6',
+                            cancelButtonColor: '#d33',
+                            confirmButtonText: 'Ya',
+                        }).then((result) => {
+                            //if user clicks on delete
+                            if (result.value) {
+                                // calling destroy method to delete
+                                @this.call('delete', locationId)
+                            }
+                        });
                     });
-                });
-            })
-        </script>
-    @endpush
-    @include('admin/kelolamaps/modaltambah')
-    @include('admin/kelolamaps/modaledit')
-</div>
+                })
+            </script>
+        @endpush
+        @include('admin/kelolamaps/modaltambah')
+        @include('admin/kelolamaps/modaledit')
+    </div>
